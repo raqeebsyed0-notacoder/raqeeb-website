9119ddd8ebc733ea9bd1498065df5bd6992d0817
Euphoria Dev <founder@euphoria-ai.co>
2026-09-18 20:35:06 +0300

    chore: remove /wall route

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,6 @@ export default function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="/wall" element={<HomePage />} />
         <Route path="/about" element={<AboutPage />} />
         <Route path="/projects" element={<ProjectsPage />} />
         <Route path="/projects/:category" element={<CategoryPage />} />
